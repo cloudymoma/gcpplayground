d1aa0fd32ebcff791438c4a90a73b402db2147d9
Bin Wu <wu.bin.msn@hotmail.com>
2015-02-15 16:48:27 +0800

    Initial setup commit

---
+++ src/main/java/org/bindiego/App.java
@@ -0,0 +1,30 @@
+package org.bindiego;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.apache.commons.configuration.ConfigurationException;
+
+import org.bindiego.util.Config;
+
+/**
+ * ServicePal image identification application
+ * http://www.faceplusplus.com/
+ */
+public class App
+{
+    private static final Logger logger =
+        LogManager.getFormatterLogger(App.class.getName());
+
+    private static PropertiesConfiguration config;
+
+    public static void main(String... args)
+    {
+        config = Config.getConfig();
+
+        logger.info(config.getProperty("app.name").toString() + " started");
+
+        logger.info(config.getProperty("app.name").toString() + " Stopped");
+    }
+}
